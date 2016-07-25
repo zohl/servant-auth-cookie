483c32478e8fd8a21b02056bbe052a2d62f086dd
Al Zohali <zohl@fmap.me>
2016-07-25 23:46:32 +0300

    nix script fixup

--- default.nix
+++ default.nix
@@ -1,7 +1,8 @@
-{ mkDerivation, base, base64-bytestring, blaze-html, blaze-markup
-, bytestring, cereal, cookie, criterion, cryptonite, deepseq, http-media
-, http-types, HUnit, memory, mtl, servant, servant-server, stdenv
-, text, time, transformers, wai, warp
+{ mkDerivation, base, base64-bytestring, blaze-builder, blaze-html
+, blaze-markup, bytestring, cereal, cookie, criterion, cryptonite
+, data-default, deepseq, exceptions, hspec, http-media, http-types
+, memory, mtl, QuickCheck, servant, servant-blaze, servant-server
+, stdenv, text, time, transformers, wai, warp
 }:
 mkDerivation {
   pname = "servant-auth-cookie";
@@ -10,16 +11,19 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    base base64-bytestring bytestring cereal cookie criterion cryptonite
-    http-types memory servant servant-server time transformers wai
+    base base64-bytestring blaze-builder bytestring cereal cookie
+    cryptonite criterion data-default exceptions http-types memory mtl servant
+    servant-server time transformers wai
   ];
   executableHaskellDepends = [
     base blaze-html blaze-markup bytestring cereal cryptonite
-    http-media mtl servant servant-server text wai warp
+    data-default http-media mtl servant servant-blaze servant-server
+    text wai warp
   ];
   testHaskellDepends = [
-    base bytestring cereal cryptonite deepseq HUnit servant-server time
+    base bytestring cereal cryptonite data-default deepseq hspec
+    QuickCheck servant-server time
   ];
   description = "Authentication via encrypted cookies";
-  license = stdenv.lib.licenses.gpl3;
+  license = stdenv.lib.licenses.bsd3;
 }
