18a9806efa6ca5c48c387826711abe198ef8003f
Arnaud Bailly <arnaud.oqube@gmail.com>
2016-09-13 10:39:24 +0200

    Provide a way to create a shared ServerKey

--- src/Servant/Server/Experimental/Auth/Cookie.hs
+++ src/Servant/Server/Experimental/Auth/Cookie.hs
@@ -36,6 +36,7 @@ module Servant.Server.Experimental.Auth.Cookie
 
   , ServerKey
   , mkServerKey
+  , mkServerKeyFromBytes
   , getServerKey
 
   , AuthCookieSettings (..)
@@ -176,6 +177,16 @@ mkServerKey :: MonadIO m
   -> m ServerKey       -- ^ New 'ServerKey'
 mkServerKey size maxAge =
   ServerKey size maxAge `liftM` liftIO (mkServerKeyState size maxAge >>= newIORef)
+
+-- | Constructor for 'ServerKey' value using predefined key.
+mkServerKeyFromBytes :: MonadIO m
+  => ByteString     -- ^ Predefined key
+  -> m ServerKey    -- ^ New 'ServerKey'
+mkServerKeyFromBytes bytes =
+  ServerKey (BS.length bytes) Nothing `liftM` liftIO (newIORef (bytes, timeOrigin)) 
+  where
+    -- we don't care about the time as the key never expires
+    timeOrigin = UTCTime (toEnum 0) 0
 
 -- | Extract value from 'ServerKey'.
 getServerKey :: MonadIO m
